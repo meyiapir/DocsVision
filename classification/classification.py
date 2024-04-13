395b39fdbec92cbb4793ca48743e546fe33ec8ba
David <kruggerdave2007@gmail.com>
2024-04-13 14:19:02 +0300

    classificator by name freq upd

--- classification/classification.py
+++ classification/classification.py
@@ -1,5 +1,6 @@
 from config import DOC_TYPES_DICT
 import csv
+from time import time
 
 
 class Classificator:
@@ -7,28 +8,42 @@ class Classificator:
         pass
 
     def find_by_name_frequency(self, text):
-        counts = []
-        r = {}
+        t1 = time()
+        r = []
+
         for i, document_type in enumerate(DOC_TYPES_DICT):
             count = text.lower().count(f'{DOC_TYPES_DICT[document_type].lower()} ')
-            r.update({tuple(DOC_TYPES_DICT.keys())[i]: count})
-            counts.append(count)
+            r.append((tuple(DOC_TYPES_DICT.keys())[i], count))
 
-        return list(DOC_TYPES_DICT.keys())[counts.index(max(counts))]
+        r.sort(reverse=True, key=lambda x: x[1])
+        counts = list(map(lambda x: x[1], r))
+        score = (counts[0] - counts[1])/(counts[0] or 1) if counts[0] else 0
+
+        if score >= 0.35:
+            return r[0][0]
+        else:
+            return None
 
 
 
 
 classificator = Classificator()
 
-total, right = 0, 0
+total, right, not_found = 0, 0, 0
 
 data = []
 with open('files/sample.csv', 'r', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
-        if classificator.find_by_name_frequency(row[1]) == row[0]:
-            right += 1
-        total += 1
+        res = classificator.find_by_name_frequency(row[1])
+        print(res)
+        if res:
+            if res == row[0]:
+                right += 1
+            total += 1
+        else:
+            not_found += 1
 
 print((right/total)*100)
+
+print(not_found)
