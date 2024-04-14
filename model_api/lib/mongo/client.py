7332ca0fa684832f22b9589089e5ab21d6e82347
David <kruggerdave2007@gmail.com>
2024-04-14 06:51:21 +0300

    mongo added

---
+++ model_api/lib/mongo/client.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from model_api.config import MONGODB_URL
+
+
+class Client:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.client = MongoClient(MONGODB_URL)
+
+    def insert(self, collection_name, data: list[dict] | dict):
+        collection_name = self.client[self.db_name][collection_name]
+        if isinstance(data, dict):
+            r = collection_name.insert_one(data)
+        elif isinstance(data, list):
+            r = collection_name.insert_many(data)
+        print(r)
+
+    def find(self, collection_name: str, filter: dict = {}):
+        return list(self.client[self.db_name][collection_name].find(filter))
+
+    def delete(self, collection_name: str, data: list[dict] | dict):
+        collection_name = self.client[self.db_name][collection_name]
+        if isinstance(data, list) or data == {}:
+            r = collection_name.delete_many(data)
+        elif isinstance(data, dict):
+            r = collection_name.delete_one(data)
